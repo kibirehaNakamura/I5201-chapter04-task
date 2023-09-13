2c250df4afb6173d7001ecc6a9557affb8b4ad42
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-13 09:52:14 +0900

    EX4_LoopWhileSum.javaを追加

---
+++ EX4_LoopWhileSum/EX4_LoopWhileSum.java
@@ -0,0 +1,12 @@
+public class EX4_LoopWhileSum {
+	public static void main(String[] args) {
+		int wa = 0;
+		int k = 5;
+		
+		while(k <= 9){
+			wa += k;
+			k++;
+		}
+		System.out.println("wa = " + wa);
+	}
+}
