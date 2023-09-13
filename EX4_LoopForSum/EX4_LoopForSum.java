63d44af524bb6fb856b694bd9c7b48d4e3e09956
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-13 09:49:39 +0900

    EX4_LoopForSum.javaを追加

---
+++ EX4_LoopForSum/EX4_LoopForSum.java
@@ -0,0 +1,10 @@
+public class EX4_LoopForSum {
+	public static void main(String[] args) {
+		int wa = 0;
+		
+		for(int k = 5; k <= 9; k++) {
+			wa += k;
+		}
+		System.out.println("wa = " + wa);
+	}
+}
